8a43ba133c3eb2361b18f830e6e8bac469e1d2ad
Yann Salaün <yann@ulule.com>
2019-10-01 15:57:10 +0200

    commit

--- main_test.go
+++ main_test.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"testing"
+	"time"
 
 	"github.com/go-redis/redis"
 )
 
 func TestMain(t *testing.T) {
 	client := redis.NewClient(&redis.Options{})
-	t.Log(client.Set("key", "value", 0).Result())
+	t.Log(client.SetNX("key", "value", 10*time.Second).Result())
 	t.Log(client.Get("key").Result())
 }
